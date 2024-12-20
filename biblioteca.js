28eefd071a6ca50a6bd5e2080549ac2631232132
zoeobertt <zoeobertt@gmail.com>
2024-12-20 13:05:41 -0300

    subo moficaciones

---
+++ biblioteca.js
@@ -0,0 +1,5 @@
+const libros = [
+{ id: 1, titulo: "Once minutos", autor: "Paulo Coelho" año: 2003, genero: "Drama", disponible: true },
+{ id: 2, titulo: "Tal vez nunca", autor: "Collen Hoover", año: 2015, genero: "Romance", disponible: false }
+
+// Para guardar + s
