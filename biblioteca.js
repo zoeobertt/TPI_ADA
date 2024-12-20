f6f0c47744cb7264a55be1d475ad1d2f268b21c9
zoeobertt <zoeobertt@gmail.com>
2024-12-20 15:26:15 -0300

    subo modificaciones

--- biblioteca.js
+++ biblioteca.js
@@ -1,5 +1,22 @@
+// Array de libros
 const libros = [
-{ id: 1, titulo: "Once minutos", autor: "Paulo Coelho" año: 2003, genero: "Drama", disponible: true },
-{ id: 2, titulo: "Tal vez nunca", autor: "Collen Hoover", año: 2015, genero: "Romance", disponible: false }
-
-// Para guardar 
+    { id: 1, titulo: "Once minutos", autor: "Paulo Coelho", año: 2003, genero: "Drama", disponible: true },
+    { id: 2, titulo: "Tal vez nunca", autor: "Colleen Hoover", año: 2015, genero: "Romance", disponible: false },
+    { id: 3, titulo: "El principito", autor: "Antoine de Saint-Exupéry", año: 1943, genero: "Fábula", disponible: true },
+    { id: 4, titulo: "Harry Potter y la piedra filosofal", autor: "J.K. Rowling", año: 1997, genero: "Fantasía", disponible: true },
+    { id: 5, titulo: "Demian", autor: "Hermann Hesse", año: 1919, genero: "Filosofía", disponible: false },
+    { id: 6, titulo: "Correr o morir", autor: "James Dashner", año: 2009, genero: "Aventura", disponible: true },
+    { id: 7, titulo: "La casa de los espíritus", autor: "Isabel Allende", año: 1982, genero: "Drama", disponible: false },
+    { id: 8, titulo: "El psicoanalista", autor: "John Katzenbach", año: 2002, genero: "Thriller", disponible: true },
+    { id: 9, titulo: "Rayuela", autor: "Julio Cortázar", año: 1963, genero: "Novela", disponible: true }
+  ];
+  
+  // Array de usuarios
+  const usuarios = [
+    { id: 1, nombre: "Zoe Obert", email: "zoeobertt@gmail.com", librosPrestados: [2, 6] },
+    { id: 2, nombre: "Mateo Alves", email: "mateo.alves@example.com", librosPrestados: [3] },
+    { id: 3, nombre: "Silvia Romariz", email: "silviaromarizz@gmail.com", librosPrestados: [8, 4] },
+    { id: 4, nombre: "Joxi Maran", email: "joxi.maran@example.com", librosPrestados: [7] }
+  ];
+  
+  
